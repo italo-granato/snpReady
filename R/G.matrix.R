5ef2854f3484d490e13f4468a529a031a0bae407
italo.granato <italo.granato@gmail.com>
2017-01-18 09:51:08 -0200

    update G.matrix

--- R/G.matrix.R
+++ R/G.matrix.R
@@ -63,7 +63,7 @@ G.matrix <- function(M, method=c("VanRaden", "UAR", "UARadj"), format=c("wide", 
     S <- ((M==2)*1) * -rep(2*(1-p)^2, each=N) + ((M==1)*1) * rep(2*p*(1-p), each=N) + ((M==0)*1) * (-rep(2*p^2, each=N))
     
     WWl <- w %*% t(w)
-    Ga <- WWl/sum(2*p*(1-p)) + diag(1e-6, nrow(WWl))
+    Ga <- WWl/(sum(diag(WWl))/N) + diag(1e-6, nrow(WWl))
     
     SSl <- S %*% t(S)
     Gd <- SSl/(sum(diag(SSl))/N)
